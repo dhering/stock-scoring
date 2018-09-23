524358d6e56d863eb4d722b54137e64097db42d1
dhering <hering.dennis@googlemail.com>
2018-09-23 12:53:19 +0200

    add setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='stock-scoring',
+      version='1.0',
+      description='',
+      author='Dennis Hering',
+      author_email='hering.dennis@googlemail.com',
+      packages=['', 'libs', 'model', 'downloader', 'scraper'],
+     )
