e1f13d8c4f05f868463c59301dea4ed9494b95a5
dhering <hering.dennis@googlemail.com>
2018-10-03 23:43:20 +0200

    extend result printing

--- runner.py
+++ runner.py
@@ -5,10 +5,13 @@ from libs.Rating import Rating
 
 task_download_index = False
 task_download = False
-task_scrap = False
+task_scrap = True
 print_full = False
 
-indexGroup = IndexGroup("DE0008467416", "DAX")
+#indexGroup = IndexGroup("DE0008469008", "DAX")
+#indexGroup = IndexGroup("DE0008467416", "MDAX")
+#indexGroup = IndexGroup("DE0007203275", "TecDAX")
+indexGroup = IndexGroup("DE0009653386", "SDAX")
 
 if task_download_index:
     downloader.dump_index(indexGroup)
@@ -36,4 +39,28 @@ for stock in indexGroup.stocks:
         if print_full:
             rating.print_overview()
 
-        print("Bewertung: %s  [%i]" % (stock.name, result))
+        if rating.is_small:
+            stock_type = "S"
+        elif rating.is_medium:
+            stock_type = "M"
+        else:
+            stock_type = "L"
+
+        if rating.is_finance:
+            stock_type += ", F"
+
+        buy_signal = ""
+        if (rating.is_small or rating.is_medium):
+            if result == 7:
+                buy_signal = "+"
+            if result > 7:
+                buy_signal = "++"
+        else:
+            if result == 4:
+                buy_signal = "+"
+            if result > 4:
+                buy_signal = "++"
+
+
+
+        print("Bewertung: %s (%s)\t[%i]\t%s" % (stock.name, stock_type, result, buy_signal))
