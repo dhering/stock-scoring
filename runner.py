bbf99acc43bb9a116d314a274e247427e1eee577
dhering <hering.dennis@googlemail.com>
2018-10-03 22:33:35 +0200

    automize creation of stock list

--- runner.py
+++ runner.py
@@ -4,43 +4,16 @@ from libs.model import IndexGroup
 from libs.Rating import Rating
 
 task_download_index = False
-task_download = True
-task_scrap = True
+task_download = False
+task_scrap = False
 print_full = False
 
-indexGroup = IndexGroup("20735", "DAX")
-indexGroup.add_stock("DE000A1EWWW0", "Adidas")
-indexGroup.add_stock("DE0008404005", "Allianz")
-indexGroup.add_stock("DE000BASF111", "BASF")
-indexGroup.add_stock("DE0005190003", "BMW")
-indexGroup.add_stock("DE000BAY0017", "Bayer")
-indexGroup.add_stock("DE0005200000", "Beiersdorf")
-indexGroup.add_stock("DE0005439004", "Continental")
-indexGroup.add_stock("DE0006062144", "COVESTRO-AG")
-indexGroup.add_stock("DE0007100000", "Daimler")
-indexGroup.add_stock("DE0005140008", "Deutsche-Bank")
-indexGroup.add_stock("DE0005810055", "Deutsche-Boerse")
-indexGroup.add_stock("DE0005552004", "Deutsche-Post")
-indexGroup.add_stock("DE0005557508", "Deutsche-Telekom")
-indexGroup.add_stock("DE000ENAG999", "EON")
-indexGroup.add_stock("DE0005785802", "Fresenius-Medical-Care")
-indexGroup.add_stock("DE0005785604", "Fresenius")
-indexGroup.add_stock("DE0006047004", "HeidelbergCement")
-indexGroup.add_stock("DE0006048432", "Henkel")
-indexGroup.add_stock("DE0006231004", "Infineon")
-indexGroup.add_stock("DE000A2E4L75", "LINDE-AG")
-indexGroup.add_stock("DE0006599905", "Merck")
-indexGroup.add_stock("DE0008430026", "Muenchener-Rueck")
-indexGroup.add_stock("DE0007037129", "RWE")
-indexGroup.add_stock("DE0007164600", "SAP")
-indexGroup.add_stock("DE0007236101", "Siemens")
-indexGroup.add_stock("DE0007500001", "ThyssenKrupp")
-indexGroup.add_stock("DE0007664039", "Volkswagen-VZ")
-indexGroup.add_stock("DE000A1ML7J1", "Vonovia")
-indexGroup.add_stock("DE0007472060", "Wirecard")
+indexGroup = IndexGroup("DE0008467416", "DAX")
 
 if task_download_index:
     downloader.dump_index(indexGroup)
+
+scraper.read_stocks(indexGroup)
 
 if task_scrap:
     scraper.scrap_index(indexGroup)
