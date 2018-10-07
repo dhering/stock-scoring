461ee19611fde109a4dd492c4849809fba943975
dhering <hering.dennis@googlemail.com>
2018-10-07 14:38:21 +0200

    check if file exists before download

--- libs/downloader/AbstractDownloader.py
+++ libs/downloader/AbstractDownloader.py
@@ -1,10 +1,13 @@
 import os
 import requests
+from requests import Response
+
 
 def getPath(filename):
     return filename
 
-def write_file_from_response(res, filename):
+
+def write_file_from_response(res: Response, filename: str):
     path = getPath(filename)
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -12,8 +15,13 @@ def write_file_from_response(res, filename):
     with open(path, "wb") as f:
         f.write(res.content)
 
-def download(url, filename):
-    r = requests.get(url, allow_redirects=True)
-    print("download into " + filename + ": " + r.url)
 
-    write_file_from_response(r, filename)
+def download(url: str, filename: str, checkFile: bool = True):
+
+    if checkFile and os.path.isfile(filename):
+        print("file still exists: " + filename)
+    else:
+        r = requests.get(url, allow_redirects=True)
+        print("download into " + filename + ": " + r.url)
+
+        write_file_from_response(r, filename)
