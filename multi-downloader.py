340b716b22595836454ddfd54428e76907b424b4
dhering <hering.dennis@googlemail.com>
2018-11-09 13:29:06 +0100

    add some more index groups

--- multi-downloader.py
+++ multi-downloader.py
@@ -18,10 +18,20 @@ indexGroup = IndexGroup("DE0008469008", "DAX")
 # indexGroup = IndexGroup("DE0007203275", "TecDAX")
 # indexGroup = IndexGroup("DE0009653386", "SDAX")
 # indexGroup = IndexGroup("EU0009658145", "EURO-STOXX-50")
+indexGroup = IndexGroup("EU0009658202", "EURO-STOXX-600")
 # indexGroup = IndexGroup("AT0000999982", "ATX")
 # indexGroup = IndexGroup("CH0009980894", "SMI")
-indexGroup = IndexGroup("US2605661048", "Dow-Jones")
+# indexGroup = IndexGroup("US2605661048", "Dow-Jones")
 # indexGroup = IndexGroup("US6311011026", "NASDAQ")
+# indexGroup = IndexGroup("JP9010C00002", "Nikkei")
+# indexGroup = IndexGroup("HK0000004322", "Hang-Seng")
+# indexGroup = IndexGroup("XC0009695252", "S&P-TSX-Composite")
+# indexGroup = IndexGroup("NL0000000107", "AEX")
+# indexGroup = IndexGroup("NO0000000021", "OBX")
+# indexGroup = IndexGroup("AT0000999677", "PTX")
+# indexGroup = IndexGroup("RU000A0JPEB3", "RTS")
+# indexGroup = IndexGroup("SE0000337842", "OMXS-30")
+# indexGroup = IndexGroup("ES0SI0000005", "IBEX-35")
 
 # date = datetime.strptime("06.11.2018", "%d.%m.%Y")
 date = datetime.now()
@@ -57,7 +67,7 @@ def thread_body(queue: Queue):
         queue.task_done()
 
 
-workers = 10
+workers = 8
 stock_queue = Queue()
 
 for i in range(workers):
