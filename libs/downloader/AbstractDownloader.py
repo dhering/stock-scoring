4c1f41d052402d412aaed589321d87566af7dbf8
dhering <hering.dennis@googlemail.com>
2019-11-02 13:51:05 +0100

    add retry option if downloaded file is empty

--- libs/downloader/AbstractDownloader.py
+++ libs/downloader/AbstractDownloader.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from requests import Response
+import time
 
 
 def getPath(filename):
@@ -16,12 +17,25 @@ def write_file_from_response(res: Response, filename: str):
         f.write(res.content)
 
 
-def download(url: str, filename: str, checkFile: bool = True):
-
-    if checkFile and os.path.isfile(filename):
+def download(url: str, filename: str, checkFile: bool = True, retry: bool = False, sleep: int = 0, maxSleep: int = 64):
+    if checkFile and os.path.isfile(filename) and os.path.getsize(filename) > 0:
         print("file still exists: " + filename)
     else:
+
+        if sleep > 0:
+            time.sleep(sleep)
+
         r = requests.get(url, allow_redirects=True)
         print("download into " + filename + ": " + r.url)
 
         write_file_from_response(r, filename)
+
+        if retry and os.path.getsize(filename) == 0:
+
+            retryTime = sleep * 2 if sleep > 0 else 1
+
+            if retryTime <= maxSleep:
+                print(f"download has been blocked, retry in %is: %s: %s" % (retryTime, filename, r.url))
+                download(url, filename, checkFile=False, retry=retry, sleep=retryTime, maxSleep=maxSleep)
+            else:
+                print(f"ERROR: unable to download after retries %s: %s" % (filename, r.url))
