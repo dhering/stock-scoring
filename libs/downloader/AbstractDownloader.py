b669d51838ecd0b57f3eec4a0ae120bf6aaed70c
dhering <hering.dennis@googlemail.com>
2019-11-10 00:10:05 +0100

    add POST download

--- libs/downloader/AbstractDownloader.py
+++ libs/downloader/AbstractDownloader.py
@@ -39,3 +39,27 @@ def download(url: str, filename: str, checkFile: bool = True, retry: bool = Fals
                 download(url, filename, checkFile=False, retry=retry, sleep=retryTime, maxSleep=maxSleep)
             else:
                 print(f"ERROR: unable to download after retries %s: %s" % (filename, r.url))
+
+
+def downloadByPost(url: str, data: dict, filename: str, checkFile: bool = True, retry: bool = False, sleep: int = 0, maxSleep: int = 64):
+    if checkFile and os.path.isfile(filename) and os.path.getsize(filename) > 0:
+        print("file still exists: " + filename)
+    else:
+
+        if sleep > 0:
+            time.sleep(sleep)
+
+        r = requests.post(url, data, allow_redirects=True)
+        print("download POST into " + filename + ": " + r.url)
+
+        write_file_from_response(r, filename)
+
+        if retry and os.path.getsize(filename) == 0:
+
+            retryTime = sleep * 2 if sleep > 0 else 1
+
+            if retryTime <= maxSleep:
+                print(f"download has been blocked, retry in %is: %s: %s" % (retryTime, filename, r.url))
+                download(url, filename, checkFile=False, retry=retry, sleep=retryTime, maxSleep=maxSleep)
+            else:
+                print(f"ERROR: unable to download after retries %s: %s" % (filename, r.url))
