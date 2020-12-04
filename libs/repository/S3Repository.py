49b2d6ed96c1bbc3a595b827a79a177164cb6d7b
dhering <hering.dennis@googlemail.com>
2020-12-04 22:21:20 +0100

    add has_content

--- libs/repository/S3Repository.py
+++ libs/repository/S3Repository.py
@@ -1,10 +1,12 @@
 import time
 
 from google.cloud import storage
+from google.cloud.exceptions import NotFound
+
 
 class S3Repository:
 
-    def __init__(self, bucket_name:str):
+    def __init__(self, bucket_name: str):
         self.bucket_name = bucket_name
 
     def store(self, path: str, content):
@@ -17,9 +19,25 @@ class S3Repository:
         time.sleep(5)
 
     def load(self, path: str):
+
+        blob = self.load_blob(path)
+
+        return blob.download_as_text()
+
+    def load_blob(self, path: str):
         storage_client = storage.Client()
         bucket = storage_client.get_bucket(self.bucket_name)
 
-        blob = bucket.blob(path)
+        return bucket.blob(path)
 
-        return blob.download_as_string()
+    def has_content(self, path: str):
+
+        try:
+            blob = self.load_blob(path)
+            content = blob.download_as_text()
+
+            if content and len(content) > 0:
+                return True
+
+        except NotFound:
+            return False
