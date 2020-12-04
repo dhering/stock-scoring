155e22f096dfa2a9a3ce5a5caf53c7ec2ab4ac60
dhering <hering.dennis@googlemail.com>
2020-12-04 22:19:34 +0100

    create missing directories when storing files

--- libs/repository/FileSystemRepository.py
+++ libs/repository/FileSystemRepository.py
@@ -1,10 +1,22 @@
+import os
+
+
 class FileSystemRepository:
+
+    def __init__(self):
+        return
+
     def store(self, path: str, content):
-        with open(path, "w") as f:
+
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        mode = "w" if isinstance(content, str) else "wb"
+
+        with open(path, mode) as f:
             f.write(content)
 
-
     def load(self, path: str):
+
         with open(path, "r") as f:
             return f.read()
 
