311197f34c5db59f453398c1af164c4aa725c1d8
dhering <hering.dennis@googlemail.com>
2020-12-04 22:22:19 +0100

    change file based downloader to storage repository

--- libs/downloader/AbstractDownloader.py
+++ libs/downloader/AbstractDownloader.py
@@ -1,24 +1,10 @@
-import os
 import requests
-from requests import Response
 import time
 
 
-def getPath(filename):
-    return filename
-
-
-def write_file_from_response(res: Response, filename: str):
-    path = getPath(filename)
-
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-
-    with open(path, "wb") as f:
-        f.write(res.content)
-
-
-def download(url: str, filename: str, checkFile: bool = True, retry: bool = False, sleep: int = 0, maxSleep: int = 64):
-    if checkFile and os.path.isfile(filename) and os.path.getsize(filename) > 0:
+def download(url: str, filename: str, storage_repository, checkFile: bool = True, retry: bool = False, sleep: int = 0,
+             maxSleep: int = 64):
+    if checkFile and storage_repository.has_content(filename):
         print("file still exists: " + filename)
     else:
 
@@ -28,21 +14,23 @@ def download(url: str, filename: str, checkFile: bool = True, retry: bool = Fals
         r = requests.get(url, allow_redirects=True)
         print("download into " + filename + ": " + r.url)
 
-        write_file_from_response(r, filename)
+        storage_repository.store(filename, r.content)
 
-        if retry and os.path.getsize(filename) == 0:
+        if retry and not storage_repository.has_content(filename):
 
             retryTime = sleep * 2 if sleep > 0 else 1
 
             if retryTime <= maxSleep:
                 print(f"download has been blocked, retry in %is: %s: %s" % (retryTime, filename, r.url))
-                download(url, filename, checkFile=False, retry=retry, sleep=retryTime, maxSleep=maxSleep)
+                download(url, filename, storage_repository, checkFile=False, retry=retry, sleep=retryTime,
+                         maxSleep=maxSleep)
             else:
                 print(f"ERROR: unable to download after retries %s: %s" % (filename, r.url))
 
 
-def downloadByPost(url: str, data: dict, filename: str, checkFile: bool = True, retry: bool = False, sleep: int = 0, maxSleep: int = 64):
-    if checkFile and os.path.isfile(filename) and os.path.getsize(filename) > 0:
+def downloadByPost(url: str, data: dict, filename: str, storage_repository, checkFile: bool = True, retry: bool = False,
+                   sleep: int = 0, maxSleep: int = 64):
+    if checkFile and storage_repository.has_content(filename):
         print("file still exists: " + filename)
     else:
 
@@ -52,14 +40,15 @@ def downloadByPost(url: str, data: dict, filename: str, checkFile: bool = True, 
         r = requests.post(url, data, allow_redirects=True)
         print("download POST into " + filename + ": " + r.url)
 
-        write_file_from_response(r, filename)
+        storage_repository.store(filename, r.content)
 
-        if retry and os.path.getsize(filename) == 0:
+        if retry and not storage_repository.has_content(filename):
 
             retryTime = sleep * 2 if sleep > 0 else 1
 
             if retryTime <= maxSleep:
                 print(f"download has been blocked, retry in %is: %s: %s" % (retryTime, filename, r.url))
-                download(url, filename, checkFile=False, retry=retry, sleep=retryTime, maxSleep=maxSleep)
+                downloadByPost(url, data, filename, storage_repository, checkFile=False, retry=retry, sleep=retryTime,
+                               maxSleep=maxSleep)
             else:
                 print(f"ERROR: unable to download after retries %s: %s" % (filename, r.url))
