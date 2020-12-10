8e9f36d6393a83f8778fda371731553932b716e8
dhering <hering.dennis@googlemail.com>
2020-12-10 21:55:40 +0100

    add deployment for stock-dumper

--- deploy-gcp.bat
+++ deploy-gcp.bat
@@ -7,4 +7,6 @@ echo == sync dist ==
 call gsutil rsync -r dist gs://stock-storage-sourcecode/dist
 
 echo == deploy dumper cloud function
-call gcloud functions deploy index-dumper-test --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=2 --trigger-topic=index-dump --entry-point=dump_index
+call gcloud functions deploy index-dumper-test --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=2 --trigger-topic=index-dump --entry-point=dump_index --set-env-vars LOG_LEVEL=INFO
+
+call gcloud functions deploy stock-dumper --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=2 --trigger-topic=stock-dump --entry-point=dump_stock
