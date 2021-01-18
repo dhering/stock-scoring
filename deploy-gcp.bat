7296b4ab52019f7341fb547a9f87e64bde7d44f4
dhering <hering.dennis@googlemail.com>
2021-01-18 23:55:01 +0100

    set instances=10 and timeout=120

--- deploy-gcp.bat
+++ deploy-gcp.bat
@@ -9,4 +9,4 @@ call gsutil rsync -r dist gs://stock-storage-sourcecode/dist
 echo == deploy dumper cloud function
 call gcloud functions deploy index-dumper-test --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=2 --trigger-topic=index-dump --entry-point=dump_index --set-env-vars LOG_LEVEL=INFO
 
-call gcloud functions deploy stock-dumper --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=2 --trigger-topic=stock-dump --entry-point=dump_stock
+call gcloud functions deploy stock-dumper --region europe-west3 --memory=256MB --runtime=python38 --source=gs://stock-storage-sourcecode/dist/gcp-dumper-function.zip --max-instances=10 --timeout=120 --trigger-topic=stock-dump --entry-point=dump_stock
